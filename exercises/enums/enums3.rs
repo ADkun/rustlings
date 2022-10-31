ea7db166204889b052550932b0fa35ba4d9a95af
adkun <adgnn@foxmail.com>
2022-10-31 18:37:15 +0800

    chore: [done] 50 - errors1

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -2,10 +2,12 @@
 // Address all the TODOs to make the tests pass!
 // Execute `rustlings hint enums3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 enum Message {
     // TODO: implement the message variant types based on their usage below
+    ChangeColor((u8, u8, u8)),
+    Echo(String),
+    Move(Point),
+    Quit,
 }
 
 struct Point {
@@ -38,6 +40,23 @@ impl State {
 
     fn process(&mut self, message: Message) {
         // TODO: create a match expression to process the different message variants
+        match message {
+            Message::ChangeColor((a, b, c)) => {
+                self.color.0 = a;
+                self.color.1 = b;
+                self.color.2 = c;
+            }
+            Message::Echo(s) => {
+                println!("{}", s);
+            }
+            Message::Move(p) => {
+                self.position.x = p.x;
+                self.position.y = p.y;
+            }
+            Message::Quit => {
+                self.quit = true;
+            }
+        }
     }
 }
 
--- exercises/hashmaps/hashmaps2.rs
+++ exercises/hashmaps/hashmaps2.rs
@@ -11,8 +11,6 @@
 //
 // Execute `rustlings hint hashmaps2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 #[derive(Hash, PartialEq, Eq)]
@@ -34,9 +32,15 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
     ];
 
     for fruit in fruit_kinds {
-        // TODO: Put new fruits if not already present. Note that you
-        // are not allowed to put any type of fruit that's already
-        // present!
+        match fruit {
+            Fruit::Apple | Fruit::Mango | Fruit::Lychee => (),
+            Fruit::Banana => {
+                basket.insert(Fruit::Banana, 10);
+            }
+            Fruit::Pineapple => {
+                basket.insert(Fruit::Pineapple, 20);
+            }
+        }
     }
 }
 
